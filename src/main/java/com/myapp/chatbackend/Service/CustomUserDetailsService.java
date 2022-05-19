5a6d0025bbc68f70dc825df914ff44976de83842
0xNullHex <boucittamansour@gmail.com>
2022-05-19 22:37:07 +0100

    Bugs Fixed. and other bugs added lol

--- src/main/java/com/myapp/chatbackend/Service/CustomUserDetailsService.java
+++ src/main/java/com/myapp/chatbackend/Service/CustomUserDetailsService.java
@@ -6,12 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-    @Autowired
+
     private UserRepository userRepository;
 
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
