7b6ec65ab1403e24659244aaa75f6ec9d45f4109
0xNullHex <boucittamansour@gmail.com>
2022-05-20 20:34:32 +0100

    Bugs Fixed. and other bugs added lol

--- src/main/java/com/myapp/chatbackend/Controller/UserCrudController.java
+++ src/main/java/com/myapp/chatbackend/Controller/UserCrudController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityNotFoundException;
 
+//TODO: review Cross Origins
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/")
 public class UserCrudController {
